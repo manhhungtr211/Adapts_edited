f2d475b31f759dc094336932837788c4daa7fde1
mhungtr211 <manhhungtr211@gmail.com>
2026-05-05 23:31:24 +0700

    define torch_dtype=16

--- src/models/model.py
+++ src/models/model.py
@@ -1,8 +1,9 @@
 from transformers import AutoModelForCausalLM
+import torch
 
 def get_model(**kwargs):
     if kwargs.pop("model_parallel")==True:
-        model = AutoModelForCausalLM.from_pretrained(device_map="auto", **kwargs)
+        model = AutoModelForCausalLM.from_pretrained(device_map="auto", torch_dtype=torch.float16, **kwargs)
     else:
-        model = AutoModelForCausalLM.from_pretrained(**kwargs)
+        model = AutoModelForCausalLM.from_pretrained(torch_dtype=torch.float16, **kwargs)
     return model
