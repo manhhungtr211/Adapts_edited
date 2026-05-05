6efa9807bacc27c6a12dc623a5ae64ed3e8d4e0b
mhungtr211 <manhhungtr211@gmail.com>
2026-05-06 00:07:22 +0700

    fix model

--- src/models/model.py
+++ src/models/model.py
@@ -2,8 +2,19 @@ from transformers import AutoModelForCausalLM
 import torch
 
 def get_model(**kwargs):
-    if kwargs.pop("model_parallel")==True:
-        model = AutoModelForCausalLM.from_pretrained(device_map="auto", torch_dtype=torch.float16, **kwargs)
+    model_parallel = kwargs.pop("model_parallel")
+    # Fix: "True" (string) != True (bool)
+    use_parallel = str(model_parallel).lower() == "true"
+    
+    if use_parallel:
+        model = AutoModelForCausalLM.from_pretrained(
+            device_map="auto",
+            torch_dtype=torch.float16,
+            **kwargs
+        )
     else:
-        model = AutoModelForCausalLM.from_pretrained(torch_dtype=torch.float16, **kwargs)
+        model = AutoModelForCausalLM.from_pretrained(
+            torch_dtype=torch.float16,
+            **kwargs
+        )
     return model
