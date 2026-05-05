b9bc37e8fb45e2d07af98d81fe1a868b137f1afe
mhungtr211 <manhhungtr211@gmail.com>
2026-05-05 23:45:08 +0700

    change multi_gpu

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -34,9 +34,9 @@ elif [ ${N_GPU} == '4' ]; then
         output_dir=${OUTPUT_DIR} res_dir=${RES_DIR} cache_dir=${CACHE_DIR} model_parallel=${model_parallel} \
         hydra.run.dir=/tmp
 elif [ ${N_GPU} == '2' ]; then
-    CUDA_VISIBLE_DEVICES='0,1' accelerate launch  --num_processes ${N_GPU} --multi_gpu \
+    CUDA_VISIBLE_DEVICES='0,1' accelerate launch --num_processes 1 \
         inference.py task_name=${TASK} model_name=${MODEL} add_bos_token=${ADD_BOS} \
-        output_dir=${OUTPUT_DIR} res_dir=${RES_DIR} cache_dir=${CACHE_DIR} model_parallel=${model_parallel} \
+        output_dir=${OUTPUT_DIR} res_dir=${RES_DIR} cache_dir=${CACHE_DIR} model_parallel=${MODEL_PARALLEL} \
         hydra.run.dir=/tmp
 elif [ ${N_GPU} == '1' ]; then
     CUDA_VISIBLE_DEVICES='0' accelerate launch  --num_processes 1 \
